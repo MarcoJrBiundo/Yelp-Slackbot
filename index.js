8d9620f7a28e08205c2339a199fdfc819d28cd9b
Marco Biundo <marcobiundo@Marcos-MacBook-Pro.local>
2020-10-01 16:47:19 -0400

    Project complete

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//"StAuth10065: I Marco Biundo, 000299457 certify that this material is my original work. No other person's work has been used without due acknowledgement. I have not made my work available to anyone else."
 const Logger = require('./lib/logger.js');
 const SlackBot = require('./lib/slack_bot.js');
 
